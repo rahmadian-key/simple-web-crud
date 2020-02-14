94b67a411625e44952ad69460f3e349f87d7eb6b
rahmadian-key <61044532+rahmadian-key@users.noreply.github.com>
2020-02-14 20:55:51 +0700

    Perubahan pada Home
    Adding some heading and paragraph

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -1,2 +1,11 @@
+<div class="container mx-auto">
+	<div class="row mt-3">
+		<div class="col-md-12">
+			<div>
+				  <h1 class="display-1">Hello, world!</h1>
+				  <p class="lead">This is a simple code igniter site.
+				  <br /><small class="text-muted">We make some simple CRUD Table transaction data.</small></p>
+			</div>
+	</div>
 
-    <h1>Hello, world!</h1>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,7 +14,7 @@
     <div class="mx-auto">
   	<div class="container">
   		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  			<a class="navbar-brand" href="#">Simple Web
+  			<a class="navbar-brand" href="<?= base_url(); ?>">Simple Web
   			</a>
   			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     			<span class="navbar-toggler-icon">
